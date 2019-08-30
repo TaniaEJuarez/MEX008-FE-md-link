d77d223c49cf7320535464dd435cd7ad062ed816
TaniaEJuarez <telizabethjdiaz@gmail.com>
2019-08-30 11:11:09 -0500

    Haciendo prueba de funcion, export e impresion en terminal

--- index.js
+++ index.js
@@ -3,32 +3,32 @@
 // };
 
 
-// const prueba = () => {
-//   console.log('hola');
-//   return 'Hola del return'
-// }
+const prueba = () => {
+  console.log('Hi Tania');
+  return 'Greeting'
+}
 
-// module.exports = prueba
+module.exports = prueba
 
-const mdLinks = require("md-links");
+// const mdLinks = require("md-links");
 
-// Caso 1 .- Ruta relativa sin options
-mdLinks("./some/example.md")
-  .then(links => {
-    // => [{ href, text, file, line }]
-  })
-  .catch(console.error);
+// // Caso 1 .- Ruta relativa sin options
+// mdLinks("./some/example.md")
+//   .then(links => {
+//     // => [{ href, text, file, line }]
+//   })
+//   .catch(console.error);
 
-// Caso  .- Ruta relativa con option (validate)
-mdLinks("./some/example.md", { validate: true })
-  .then(links => {
-    // => [{ href, text, file, status, ok }]
-  })
-  .catch(console.error);
+// // Caso  .- Ruta relativa con option (validate)
+// mdLinks("./some/example.md", { validate: true })
+//   .then(links => {
+//     // => [{ href, text, file, status, ok }]
+//   })
+//   .catch(console.error);
 
-// Caso 3 .- Ruta relativa de un directorio sin options
-mdLinks("./some/dir")
-  .then(links => {
-    // => [{ href, text, file, line }]
-  })
-  .catch(console.error);
+// // Caso 3 .- Ruta relativa de un directorio sin options
+// mdLinks("./some/dir")
+//   .then(links => {
+//     // => [{ href, text, file, line }]
+//   })
+//   .catch(console.error);
