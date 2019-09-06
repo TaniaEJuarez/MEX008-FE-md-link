e9fcfb6a59cd08548d69718cfbe603ee88080756
TaniaEJuarez <telizabethjdiaz@gmail.com>
2019-09-06 16:24:22 -0500

    Declarando modulo para identificar URL para devolver las direcciones URL completas

--- searchCount.js
+++ searchCount.js
@@ -1,11 +1,11 @@
 // Identifica y lee los url contenidos en el archivo y devuelve solo el numero de url contenidas.
 // Para ejecutar, indicar node searchCount.js (que es el nombre del archivo) y 
 // /home/tjuarez/Documentos/PROYECTOS/MdLinks/MEX008-FE-md-link/READMEPaths.md (que es el path del archivo que voy a leer)
-let fs = require('fs');
-let file = process.argv[2] // guardar el arreglo en una variable. indicar a partir de que posicion inicia el path //  
-let buf = fs.readFileSync(file); // Declarar una variable para guardar el buffer que nos resulta de fs //
-let str = buf.toString(); // pasar el buffer reultante a un string //
-console.log(str.match(new RegExp('https://', 'g')).length); // la propiedad match, identifica coincidencias, asi mismo necesita el argumento de la letra g para indicar que quiero me lo busque en todo el archivo //s
+// let fs = require('fs');
+// let file = process.argv[2] // guardar el arreglo en una variable. indicar a partir de que posicion inicia el path //  
+// let buf = fs.readFileSync(file); // Declarar una variable para guardar el buffer que nos resulta de fs //
+// let str = buf.toString(); // pasar el buffer reultante a un string //
+// console.log(str.match(new RegExp('https://', 'g')).length); // la propiedad match, identifica coincidencias, asi mismo necesita el argumento de la letra g para indicar que quiero me lo busque en todo el archivo //s
 
 
 
@@ -14,8 +14,10 @@ console.log(str.match(new RegExp('https://', 'g')).length); // la propiedad matc
 // Identifica y lee los url contenidos en el archivo y devuelve las direcciones completas.
 // Para ejecutar, indicar node searchCount.js (que es el nombre del archivo) y 
 // /home/tjuarez/Documentos/PROYECTOS/MdLinks/MEX008-FE-md-link/READMEPaths.md ( que es el path del archivo que voy a leer)
-    // let fs = require('fs');
-    // let file = process.argv[2] // guardar el arreglo en una variable. indicar a partir de que posicion inicia el path //
-    // let buf = fs.readFileSync(file);  // Declarar una variable para guardar el buffer que nos resulta de fs //
-    // let str = buf.toString(); // pasar el buffer reultante a un string //
-    // console.log(str.match(new RegExp('https://', 'g'))); // la propiedad match, identifica coincidencias, asi mismo necesita el argumento de la letra g para indicar que quiero me lo busque en todo el archivo //
+    let fs = require('fs');
+    let file = process.argv[2] // guardar el arreglo en una variable. indicar a partir de que posicion inicia el path //
+    let buf = fs.readFileSync(file);  // Declarar una variable para guardar el buffer que nos resulta de fs //
+    let str = buf.toString(); // pasar el buffer reultante a un string //
+    console.log(str.match(new RegExp('https://', 'g'))); // la propiedad match, identifica coincidencias, asi mismo necesita el argumento de la letra g para indicar que quiero me lo busque en todo el archivo //
+
+// LEER EL ARTICULO DE EXPRESIONES REGULARES PARA QUE EL ARCHIVO DEVUELVA LAS DIRECCIONES COMPLETAS //
